4666d33589e62f4cb9e4edb7a6bffa540609e435
zpr <Dennis.Zeipert@streetscooter.eu>
2018-03-11 13:17:38 +0100

    added some comments to model.py

--- model.py
+++ model.py
@@ -14,8 +14,7 @@ import numpy as np
 lines = []
 
 
-
-with open('/media/zpr/5aa7062e-a1a2-4b29-85cb-5756318d57ee/Udacity/CarND-Behavioral-Cloning-P3/Udacity/driving_log.csv') as csvfile:
+with open('/Udacity/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -27,13 +26,14 @@ for line in lines:
     if n > 0:
         source_path = line[0]
         filename = source_path.split('/')[-1]
-        current_path = '/media/zpr/5aa7062e-a1a2-4b29-85cb-5756318d57ee/Udacity/CarND-Behavioral-Cloning-P3/Udacity/IMG/' + filename
+        current_path = '/Udacity/IMG/' + filename
         image = cv2.imread(current_path)
         images.append(image)
         measurement = float(line[3])
         measurements.append(measurement)
     n+=1
 
+# flip every image before giving it to the network to train
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -47,27 +47,23 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)    
     
 
-
+# Convolutional Network archtecture starts here
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Cropping2D, Dropout
 from keras import optimizers
 
 model = Sequential()
-model.add(Lambda(lambda x: x/255 -0.5,input_shape=(160,320,3) ))
+model.add(Lambda(lambda x: x/255 -0.5,input_shape=(160,320,3) )) # normalize image values to range between 0 and 1
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(6, (5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(4, 4)))
-#model.add(Convolution2D(6, (3, 3), activation='elu'))
-#model.add(Dropout(0.25))
+model.add(Convolution2D(6, (3, 3), activation='relu')) # relu nonlinearity as activation of convolutional layer with kernel size of 3x3
+model.add(MaxPooling2D(pool_size=(4, 4))) # maxPooling with 4x4 kernel size to reduze information to the most important aspects
+model.add(Dropout(0.25))    # dropout with 25 % probability to prevent overfitting
 model.add(Flatten())
-#model.add(Dense(10))
-model.add(Dense(1, activation = 'tanh'))
+model.add(Dense(1, activation = 'tanh'))    # fullyConnected layer as output with tanh activation to get values between 0 and 1
 model.summary()
 
-adm = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-
-model.compile(loss = 'mse', optimizer = adm , metrics=['accuracy'])
+model.compile(loss = 'mse', optimizer = 'Adam' , metrics=['accuracy'])
 model.fit(X_train, y_train, validation_split = 0.2, shuffle = False, nb_epoch = 2, batch_size = 32)
 
-model.save('model_lower_learning_rate_alte_daten_layer.h5')
+model.save('model.h5')
